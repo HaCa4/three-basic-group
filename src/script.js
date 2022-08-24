41946024cbbbb10348e446810b4c2a18d90cad23
Çağrı <hcozagar@gmail.com>
2022-08-24 23:13:31 +0300

    simpler code

--- src/script.js
+++ src/script.js
@@ -1,23 +1,22 @@
 import "./style.css";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { FontLoader } from "three/examples/jsm/loaders/FontLoader";
+import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry";
+
+console.time("tick");
+const clock = new THREE.Clock();
 
 const textureLoader = new THREE.TextureLoader();
 
-const sTexture1 = textureLoader.load("/1.jpg");
-sTexture1.minFilter = THREE.NearestFilter;
+const sTextureOthers = textureLoader.load("/textures/1.jpg");
+sTextureOthers.minFilter = THREE.NearestFilter;
 
-const sTexture2 = textureLoader.load("/2.jpg");
-sTexture1.minFilter = THREE.NearestFilter;
+const sTextureCenter = textureLoader.load("/textures/2.jpg");
+sTextureCenter.minFilter = THREE.NearestFilter;
 
-const sTexture3 = textureLoader.load("/3.jpg");
-sTexture1.minFilter = THREE.NearestFilter;
-
-const sTexture4 = textureLoader.load("/4.jpg");
-sTexture1.minFilter = THREE.NearestFilter;
-
-const sTexture5 = textureLoader.load("/5.jpg");
-sTexture1.minFilter = THREE.NearestFilter;
+const matcapTexture = textureLoader.load("/textures/8.png");
+const textTexture = textureLoader.load("/textures/5.png");
 
 //BASE
 
@@ -28,55 +27,82 @@ const scene = new THREE.Scene();
 const sepGroup = new THREE.Group();
 scene.add(sepGroup);
 
-const s1 = new THREE.Mesh(
-  new THREE.SphereGeometry(1, 32, 1100),
-  new THREE.MeshLambertMaterial({ map: sTexture1 })
-);
-
-const s2 = new THREE.Mesh(
-  new THREE.SphereGeometry(1, 32, 1100),
-  new THREE.MeshLambertMaterial({ map: sTexture2 })
-);
-
-const s3 = new THREE.Mesh(
+const sCenter = new THREE.Mesh(
   new THREE.SphereGeometry(2, 32, 1100),
-  new THREE.MeshLambertMaterial({ map: sTexture3 })
+  new THREE.MeshPhongMaterial({ map: sTextureCenter })
 );
+sCenter.position.x = 0;
+sCenter.material.shininess = 30;
+sCenter.material.specular = 0xff0000;
+scene.add(sCenter);
 
-const s4 = new THREE.Mesh(
-  new THREE.SphereGeometry(1, 32, 1101),
-  new THREE.MeshLambertMaterial({ map: sTexture4 })
-);
+const geoS = new THREE.SphereGeometry(0.5, 32, 1100);
+const mat = new THREE.MeshLambertMaterial({ map: sTextureOthers });
 
-const s5 = new THREE.Mesh(
-  new THREE.SphereGeometry(1, 32, 1100),
-  new THREE.MeshLambertMaterial({ map: sTexture5 })
-);
+let s1, s2, s3, s4, s5, s6;
+const arr = [s1, s2, s3, s4, s5, s6];
+for (let i = 0; i < 6; i++) {
+  arr[i] = new THREE.Mesh(geoS, mat);
+  sepGroup.add(arr[i]);
+}
+arr[0].position.x = -5;
+arr[1].position.x = 5;
+arr[2].position.y = -5;
+arr[3].position.y = 5;
+arr[4].position.z = -5;
+arr[5].position.z = 5;
 
-s1.position.x = -5;
-s1.position.z = 2;
-s2.position.x = 5;
-s2.position.z = 2;
-s3.position.x = 0;
-s3.position.z = -3;
-s4.position.y = 5;
-s5.position.y = -5;
-s5.position.z = -6;
+let pointColor = new THREE.Color("white");
+let ambientColor = new THREE.Color("rgb(155,80,155)");
 
-sepGroup.add(s1);
-sepGroup.add(s2);
-sepGroup.add(s3);
-sepGroup.add(s4);
-sepGroup.add(s5);
-
-const ambientLight = new THREE.AmbientLight(0xff0, 0.4);
+const ambientLight = new THREE.AmbientLight(ambientColor, 0.5);
 scene.add(ambientLight);
 
-const pointLight = new THREE.PointLight(0xff00ff, 1);
-pointLight.position.x = 8;
-pointLight.position.y = 8;
-pointLight.position.z = 0;
+const pointLight = new THREE.PointLight(pointColor, 0.8);
+pointLight.position.x = -10;
+pointLight.position.y = 4;
+pointLight.position.z = 8;
 scene.add(pointLight);
+
+/**
+ * FONT LOADER
+ */
+
+const fontLoader = new FontLoader();
+fontLoader.load("/fonts/helvetiker_regular.typeface.json", (font) => {
+  const textGeometry = new TextGeometry(
+    "Planet Trial", //Text
+    {
+      font: font,
+      size: 0.5,
+      height: 0.2, //depth
+      curveSegments: 30, // surface curve
+      bevelEnabled: true, //eğim
+      bevelThickness: 0.04, //depth
+      bevelSize: 0.03,
+      bevelOffset: 0.01,
+      bevelSegments: 2,
+    }
+  );
+
+  const material = new THREE.MeshMatcapMaterial({ color: 0xffff00 });
+  const material2 = new THREE.MeshMatcapMaterial({ matcap: textTexture });
+
+  const text = new THREE.Mesh(textGeometry, material2);
+  scene.add(text);
+
+  const littleSphereGeometry = new THREE.SphereGeometry(0.5, 500, 1000);
+
+  for (let i = 0; i < 500; i++) {
+    const littleSphere = new THREE.Mesh(littleSphereGeometry, material);
+
+    littleSphere.position.x = (Math.random() - 0.5) * 1000;
+    littleSphere.position.y = (Math.random() - 0.5) * 1000;
+    littleSphere.position.z = (Math.random() - 0.5) * 1000;
+
+    scene.add(littleSphere);
+  }
+});
 
 const sizes = {
   width: window.innerWidth,
@@ -93,18 +119,19 @@ window.addEventListener("resize", () => {
 });
 
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-camera.position.z = 11;
+camera.position.x = 5;
+camera.position.y = 4;
+camera.position.z = 5;
 scene.add(camera);
 
 const controls = new OrbitControls(camera, canvas);
+controls.damping = true;
 
 const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
-controls.damping = true;
-
-const clock = new THREE.Clock();
+controls.enableDamping = true;
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
@@ -112,129 +139,17 @@ const tick = () => {
   controls.update();
 
   sepGroup.rotation.y = -elapsedTime / 10;
-  sepGroup.rotation.x = elapsedTime / 15;
+  sepGroup.rotation.x = elapsedTime / 10;
   sepGroup.rotation.Z = -elapsedTime / 10;
 
-  s1.rotation.y = elapsedTime / 3;
-  s2.rotation.y = elapsedTime / 4;
-  s3.rotation.y = elapsedTime / 5;
-  s4.rotation.y = elapsedTime / 4;
-  s5.rotation.y = elapsedTime / 3;
+  sCenter.rotation.y = elapsedTime / 5;
+  for (let i = 0; i < 6; i++) {
+    arr[i].rotation.z = elapsedTime / 3;
+    arr[i].rotation.x = -elapsedTime / 3;
+  }
 
   renderer.render(scene, camera);
   window.requestAnimationFrame(tick);
 };
 tick();
-
-/* 
-
-****************GUI EXAMPLE CODE*****************
-
-var gui = new dat.GUI({ load: getPresetJSON(), preset: 'Preset1' });
-
-var object1 = {
-	type1_boolean: false,
-  type2_string: 'string',
-  type3_number: 0,
-  type4_function: function () {
-  	alert('This is a function.');
-  }
-};
-
-var object2 = {
-	string1: 'string1',
-  string2: 'string2'
-};
-
-var object3 = {
-	color0: "#ffae23", // CSS string
-  color1: [ 0, 128, 255 ], // RGB array
-  color2: [ 0, 128, 255, 0.3 ], // RGB with alpha
-  color3: { h: 350, s: 0.9, v: 0.3 } // Hue, saturation, value
-};
-// dat.GUI will modify colors in the format defined by their initial value.
-
-// saveValues: gui.remember must be executed before gui.add
-gui.remember(object1);
-gui.remember(object2);
-
-// setController: boolean, string, number, function
-gui.add(object1, 'type1_boolean');
-gui.add(object1, 'type2_string');
-
-var folder1 = gui.addFolder('FolderNameA');
-folder1.add(object1, 'type3_number', -5, 5);
-folder1.add(object1, 'type4_function');
-
-// collapse folder1
-folder1.close();
-
-var folder2 = gui.addFolder('FolderNameB');
-folder2.add(object2, 'string1', {
-	'key1': 'string_1',
-  'key2': 'string_2',
-  'key3': 'string_3'
-});
-folder2.add(object2, 'string2', [
-	'string_1', 'string_2', 'string_3'
-]);
-
-var folder3 = gui.addFolder('FolderNameC');
-folder3.addColor(object3, 'color0');
-folder3.addColor(object3, 'color1');
-folder3.addColor(object3, 'color2');
-folder3.addColor(object3, 'color3');
-
-// expand folder3
-folder3.open();
-
-// presetJSON: created from pressing the gear.
-function getPresetJSON() {
-	return {
-    "preset": "Default",
-    "closed": false,
-    "remembered": {
-      "Default": {
-        "0": {
-          "type1_boolean": false,
-          "type2_string": "string",
-          "type3_number": 0
-        },
-        "1": {
-          "string1": "string1",
-          "string2": "string2"
-        }
-      },
-      "Preset1": {
-        "0": {
-          "type1_boolean": true,
-          "type2_string": "string123",
-          "type3_number": -2.2938689217758985
-        },
-        "1": {
-          "string1": "string_2",
-          "string2": "string_3"
-        }
-      }
-    },
-    "folders": {
-      "FolderNameA": {
-        "preset": "Default",
-        "closed": false,
-        "folders": {}
-      },
-      "FolderNameB": {
-        "preset": "Default",
-        "closed": false,
-        "folders": {}
-      },
-      "FolderNameC": {
-        "preset": "Default",
-        "closed": false,
-        "folders": {}
-      }
-    }
-  };
-}
-
-*/
+console.timeEnd("tick");
