346c4ee1384d02e9c3190c63ac1aa4c42975aa01
Çağrı <hcozagar@gmail.com>
2022-08-25 16:22:04 +0300

    performance

--- src/script.js
+++ src/script.js
@@ -4,7 +4,6 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { FontLoader } from "three/examples/jsm/loaders/FontLoader";
 import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry";
 
-console.time("Rendering Time");
 const clock = new THREE.Clock();
 
 const textureLoader = new THREE.TextureLoader();
@@ -14,8 +13,6 @@ sTextureOthers.minFilter = THREE.NearestFilter;
 
 const sTextureCenter = textureLoader.load("/textures/2.jpg");
 sTextureCenter.minFilter = THREE.NearestFilter;
-
-const textTexture = textureLoader.load("/textures/5.png");
 
 //BASE
 
@@ -28,35 +25,35 @@ scene.add(sepGroup);
 
 const sCenter = new THREE.Mesh(
   new THREE.SphereGeometry(2, 32, 1100),
-  new THREE.MeshPhongMaterial({ map: sTextureCenter })
+  new THREE.MeshPhongMaterial({ map: sTextureOthers })
 );
 sCenter.position.x = 0;
-sCenter.material.shininess = 30;
+sCenter.material.shininess = 1;
 scene.add(sCenter);
 
 const geoS = new THREE.SphereGeometry(0.5, 32, 1100);
-const mat = new THREE.MeshLambertMaterial({ map: sTextureOthers });
+const mat = new THREE.MeshLambertMaterial({ map: sTextureCenter });
 
-let s1, s2, s3, s4, s5, s6;
-const arr = [s1, s2, s3, s4, s5, s6];
-for (let i = 0; i < 6; i++) {
+let s1, s2, s3, s4;
+const arr = [s1, s2, s3, s4];
+for (let i = 0; i < 4; i++) {
   arr[i] = new THREE.Mesh(geoS, mat);
   sepGroup.add(arr[i]);
 }
 arr[0].position.x = -5;
 arr[1].position.x = 5;
-arr[2].position.y = -5;
-arr[3].position.y = 5;
-arr[4].position.z = -5;
-arr[5].position.z = 5;
+arr[2].position.z = 5;
+arr[2].position.y = -3;
+arr[3].position.z = -5;
+arr[3].position.y = 3;
 
 let pointColor = new THREE.Color("white");
 let ambientColor = new THREE.Color("rgb(155,80,155)");
 
-const ambientLight = new THREE.AmbientLight(ambientColor, 0.5);
+const ambientLight = new THREE.AmbientLight(ambientColor, 0.8);
 scene.add(ambientLight);
 
-const pointLight = new THREE.PointLight(pointColor, 0.8);
+const pointLight = new THREE.PointLight(pointColor, 2);
 pointLight.position.x = -10;
 pointLight.position.y = 4;
 pointLight.position.z = 8;
@@ -81,14 +78,14 @@ fontLoader.load("/fonts/helvetiker_regular.typeface.json", (font) => {
   });
 
   const material = new THREE.MeshMatcapMaterial({ color: 0xffff00 });
-  const material2 = new THREE.MeshMatcapMaterial({ matcap: textTexture });
+  const material2 = new THREE.MeshMatcapMaterial({ color: new THREE.Color("orange") });
 
   const text = new THREE.Mesh(textGeometry, material2);
   scene.add(text);
 
   const littleSphereGeometry = new THREE.SphereGeometry(0.5, 500, 1000);
 
-  for (let i = 0; i < 500; i++) {
+  for (let i = 0; i < 150; i++) {
     const littleSphere = new THREE.Mesh(littleSphereGeometry, material);
 
     littleSphere.position.x = (Math.random() - 0.5) * 1000;
@@ -133,18 +130,17 @@ const tick = () => {
 
   controls.update();
 
-  sepGroup.rotation.y = -elapsedTime / 10;
-  sepGroup.rotation.x = elapsedTime / 10;
-  sepGroup.rotation.Z = -elapsedTime / 10;
+  sepGroup.rotation.y = -elapsedTime / 20;
+  sepGroup.rotation.x = elapsedTime / 20;
+  sepGroup.rotation.Z = -elapsedTime / 20;
 
-  sCenter.rotation.y = elapsedTime / 5;
-  for (let i = 0; i < 6; i++) {
-    arr[i].rotation.z = elapsedTime / 3;
-    arr[i].rotation.x = -elapsedTime / 3;
+  sCenter.rotation.y = elapsedTime / 25;
+  for (let i = 0; i < 4; i++) {
+    arr[i].rotation.z = elapsedTime / 10;
+    arr[i].rotation.x = -elapsedTime / 10;
   }
 
   renderer.render(scene, camera);
   window.requestAnimationFrame(tick);
 };
 tick();
-console.timeEnd("tick");
