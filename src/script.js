f18378fdc93589f814d007c4ab7e0474262e4d78
Çağrı <hcozagar@gmail.com>
2022-08-29 13:41:10 +0300

    shadows

--- src/script.js
+++ src/script.js
@@ -24,39 +24,47 @@ const sepGroup = new THREE.Group();
 scene.add(sepGroup);
 
 const sCenter = new THREE.Mesh(
-  new THREE.SphereGeometry(2, 32, 1100),
+  new THREE.SphereGeometry(3, 32, 1100),
   new THREE.MeshPhongMaterial({ map: sTextureOthers })
 );
 sCenter.position.x = 0;
 sCenter.material.shininess = 1;
+sCenter.receiveShadow = true;
+sCenter.castShadow = true;
 scene.add(sCenter);
 
-const geoS = new THREE.SphereGeometry(0.5, 32, 1100);
+const geoS = new THREE.SphereGeometry(0.3, 32, 1100);
 const mat = new THREE.MeshLambertMaterial({ map: sTextureCenter });
 
-let s1, s2, s3, s4;
-const arr = [s1, s2, s3, s4];
-for (let i = 0; i < 4; i++) {
+let s1, s2, s3, s4, s5, s6;
+const arr = [s1, s2, s3, s4, s5, s6];
+for (let i = 0; i < 6; i++) {
   arr[i] = new THREE.Mesh(geoS, mat);
+  arr[i].castShadow = true;
+  arr[i].receiveShadow = true;
   sepGroup.add(arr[i]);
 }
-arr[0].position.x = -5;
-arr[1].position.x = 5;
-arr[2].position.z = 5;
-arr[2].position.y = -3;
-arr[3].position.z = -5;
-arr[3].position.y = 3;
+
+arr[0].position.x = 4.5;
+arr[1].position.x = -4.5;
+arr[2].position.y = 4.5;
+arr[3].position.y = -4.5;
+arr[4].position.z = 4.5;
+arr[5].position.z = -4.5;
 
 let pointColor = new THREE.Color("white");
 let ambientColor = new THREE.Color("rgb(155,80,155)");
 
-const ambientLight = new THREE.AmbientLight(ambientColor, 0.8);
+const ambientLight = new THREE.AmbientLight(ambientColor, 1);
 scene.add(ambientLight);
 
-const pointLight = new THREE.PointLight(pointColor, 2);
-pointLight.position.x = -10;
-pointLight.position.y = 4;
-pointLight.position.z = 8;
+const pointLight = new THREE.PointLight(pointColor, 4);
+pointLight.position.set(10, 0, 0);
+pointLight.castShadow = true;
+pointLight.shadow.camera.far = 15;
+pointLight.shadow.camera.near = 5;
+pointLight.shadow.mapSize.set(1024, 1024);
+
 scene.add(pointLight);
 
 /**
@@ -65,7 +73,7 @@ scene.add(pointLight);
 
 const fontLoader = new FontLoader();
 fontLoader.load("/fonts/helvetiker_regular.typeface.json", (font) => {
-  const textGeometry = new TextGeometry("Planet Test - 1", {
+  const textGeometry = new TextGeometry("Planet                     Test - 1", {
     font: font,
     size: 0.5,
     height: 0.2,
@@ -83,7 +91,7 @@ fontLoader.load("/fonts/helvetiker_regular.typeface.json", (font) => {
   const text = new THREE.Mesh(textGeometry, material2);
   scene.add(text);
 
-  const littleSphereGeometry = new THREE.SphereGeometry(0.5, 500, 1000);
+  const littleSphereGeometry = new THREE.SphereGeometry(0.3, 500, 1000);
 
   for (let i = 0; i < 150; i++) {
     const littleSphere = new THREE.Mesh(littleSphereGeometry, material);
@@ -111,9 +119,7 @@ window.addEventListener("resize", () => {
 });
 
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-camera.position.x = 7;
-camera.position.y = 5;
-camera.position.z = 8;
+camera.position.set(12, 2, 6);
 scene.add(camera);
 
 const controls = new OrbitControls(camera, canvas);
@@ -122,7 +128,7 @@ controls.damping = true;
 const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-
+renderer.shadowMap.enabled = true;
 controls.enableDamping = true;
 
 const tick = () => {
@@ -130,14 +136,11 @@ const tick = () => {
 
   controls.update();
 
-  sepGroup.rotation.y = -elapsedTime / 20;
-  sepGroup.rotation.x = elapsedTime / 20;
-  sepGroup.rotation.Z = -elapsedTime / 20;
-
+  sepGroup.rotation.y = -elapsedTime / 10;
+  sepGroup.rotation.z = -elapsedTime / 10;
   sCenter.rotation.y = elapsedTime / 25;
-  for (let i = 0; i < 4; i++) {
-    arr[i].rotation.z = elapsedTime / 10;
-    arr[i].rotation.x = -elapsedTime / 10;
+  for (let i = 0; i < 6; i++) {
+    arr[i].rotation.x = elapsedTime / 5;
   }
 
   renderer.render(scene, camera);
